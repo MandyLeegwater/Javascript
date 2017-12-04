efd831d72f3609ec4b05537b8ec849a2a262f3f1
m-lee <m-leegwater@hotmail.com>
2017-12-04 18:20:24 +0100

    DOM JavaScript

---
+++ DOM JavaScript/2.2/stijl.js
@@ -0,0 +1,17 @@
+/**
+ * Created by m-lee on 4-12-2017.
+ */
+
+document.getElementById("bl1").style.backgroundColor = "lightblue";
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl1").style.paddingTop = "10px";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "10px";
+document.getElementById("bl2").style.textAlign = "center";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "20px";
+document.getElementById("bl3").style.textAlign = "right";
+
+document.getElementById("bl4").style.backgroundColor = "red";
